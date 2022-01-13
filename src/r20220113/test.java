27f381cf5da73e6cb240dff5009f2190162329cf
xianyu <ahod@doco.com>
2022-01-13 15:28:46 +0800

    1.2

--- src/r20220113/test.java
+++ src/r20220113/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("20220113test");
         System.out.println("再次测试一下");
+        System.out.println("下午3：25");
     }
 }
